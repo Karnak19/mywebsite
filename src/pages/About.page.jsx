6ca809a9784729e14fb446cec944fa1b58ac697f
Basile <basile64.v@gmail.com>
2019-01-09 14:09:30 +0100

    fetching resources from raider.io API to get my character sheet

--- src/pages/About.page.jsx
+++ src/pages/About.page.jsx
@@ -5,12 +5,17 @@ import {
    Nav,
    NavItem,
    NavLink,
-   Button,
    Row,
-   Col
+   Col,
+   UncontrolledTooltip,
+   Progress,
+   Card,
+   CardText,
+   CardTitle
 } from "reactstrap";
 import classnames from "classnames";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import axios from "axios";
 
 import ResponsiveLayout from "../layouts/Responsive.layout.jsx";
 import styles from "./About.page.module.css";
@@ -21,17 +26,21 @@ export default class About extends React.Component {
 
       this.toggle = this.toggle.bind(this);
       this.state = {
+         tooltipOpen: false,
          activeTab: "1",
          tabs: [
             {
                id: "1",
-               label: "World of Warcraft",
-               imgLink: "./wow.png",
-               bg: ""
+               label: "World of Warcraft"
             },
             { id: "2", label: "Ice Hockey", imgLink: "" },
             { id: "3", label: "Snowboard", imgLink: "" }
-         ]
+         ],
+         raiderIo: [],
+         bestRuns: [],
+         myProgress: [],
+         isPending: false,
+         isError: false
       };
    }
 
@@ -42,7 +51,42 @@ export default class About extends React.Component {
          });
       }
    }
+
+   componentDidMount() {
+      this.setState({ isPending: true });
+      axios
+         .get(
+            "https://raider.io/api/v1/characters/profile?region=eu&realm=hyjal&name=raquette&fields=mythic_plus_best_runs",
+            { headers: { Accept: "application/json" } }
+         )
+         .then(response => {
+            this.setState({
+               raiderIo: response.data,
+               bestRuns: response.data.mythic_plus_best_runs,
+               isPending: false
+            });
+            console.log(response.data.mythic_plus_best_runs);
+         })
+         .catch(() => this.setState({ isError: true }));
+      axios
+         .get(
+            "https://raider.io/api/v1/characters/profile?region=eu&realm=hyjal&name=raquette&fields=raid_progression",
+            { headers: { Accept: "application/json" } }
+         )
+         .then(response => {
+            this.setState({
+               myProgress: response.data.raid_progression,
+               isPending: false
+            });
+            console.log(response.data.raid_progression);
+         })
+         .catch(() => this.setState({ isError: true }));
+   }
+
    render() {
+      if (this.state.isPending) {
+         return <ResponsiveLayout>Fancy spinner here</ResponsiveLayout>;
+      }
       return (
          <ResponsiveLayout>
             <section className={styles.container}>
@@ -68,27 +112,76 @@ export default class About extends React.Component {
                   activeTab={this.state.activeTab}
                   style={{ overflowY: "auto", overflowX: "hidden" }}
                >
-                  {this.state.tabs.map(tabs => (
-                     <TabPane
-                        id={tabs.label}
-                        tabId={tabs.id}
-                        className={styles.content}
-                        style={{
-                           backgroundImage: "../assets/images/alliance.png"
-                        }}
-                     >
-                        <Row>
-                           <Col sm="12">
-                              <h2>{tabs.label}</h2>
-                              <img src={tabs.imgLink} alt="image" />
-                           </Col>
-                        </Row>
-                     </TabPane>
-                  ))}
-                  {/* <TabPane tabId="1">
+                  <TabPane id="1" tabId="1" className={styles.content}>
                      <Row>
-                        <Col sm="12">
-                           <h4>WoW</h4>
+                        <Col lg="4" sm="12" xs="12">
+                           <p>
+                              World of Warcraft is a great game. I mostly enjoy
+                              the End-game content, particularly{" "}
+                              <span href="#" id="pveTooltip">
+                                 PvE
+                              </span>
+                              . In End-game PvE, you need a lot of teamwork, to
+                              make a 20-man team cooperate in the same way.
+                              Being selfish does not work in this environment,
+                              as every player here will play a huge role to
+                              defeat each bosses.
+                           </p>
+                        </Col>
+                        <Col
+                           lg="8"
+                           sm="12"
+                           xs="12"
+                           style={{ listStyle: "none" }}
+                        >
+                           <h3>My Character :</h3>
+                           <li>
+                              <a
+                                 href={this.state.raiderIo.profile_url}
+                                 target="_blank"
+                                 rel="noopener noreferrer"
+                              >
+                                 {this.state.raiderIo.name}
+                              </a>
+                           </li>
+                           <li>
+                              <img
+                                 src={this.state.raiderIo.thumbnail_url}
+                                 alt={this.state.raiderIo.name}
+                              />
+                           </li>
+                           <li>
+                              {this.state.raiderIo.race}{" "}
+                              {this.state.raiderIo.class}
+                           </li>
+                           <li>{this.state.raiderIo.faction} proud !</li>
+                           <Progress
+                              animated
+                              className={styles.progress}
+                              value="7"
+                              max="8"
+                              size="3"
+                           >
+                              Mythic Uldir : 7/8
+                           </Progress>
+                           <Row style={{ marginTop: "20px" }}>
+                              {this.state.bestRuns.map((runs, i) => (
+                                 <Col sm="4" key={i}>
+                                    <Card body>
+                                       <CardTitle>
+                                          <a
+                                             href={runs.url}
+                                             target="_blank"
+                                             rel="noopener norefferer"
+                                          >
+                                             {runs.dungeon} {runs.mythic_level}
+                                          </a>
+                                       </CardTitle>
+                                       <CardText>Score : {runs.score}</CardText>
+                                    </Card>
+                                 </Col>
+                              ))}
+                           </Row>
                         </Col>
                      </Row>
                   </TabPane>
@@ -97,11 +190,22 @@ export default class About extends React.Component {
                         <Col sm="12">
                            <h4>Soon</h4>
                         </Col>
+                     </Row>
+                  </TabPane>
+                  <TabPane tabId="3">
+                     <Row>
+                        <Col sm="12">
+                           <h4>Soon</h4>
+                        </Col>
                         <Col sm="6" />
                      </Row>
-                  </TabPane> */}
+                  </TabPane>
                </TabContent>
             </section>
+            <UncontrolledTooltip placement="bottom" target="pveTooltip">
+               Player versus Environment : Fighting monsters (which are bots in
+               fact), alone or in group.
+            </UncontrolledTooltip>
          </ResponsiveLayout>
       );
    }
