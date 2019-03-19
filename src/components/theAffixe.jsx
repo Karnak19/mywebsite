c1f9f23064c94640355de87203bb2f39b3eb4ec0
Basile <basile64.v@gmail.com>
2019-03-19 11:35:12 +0100

    moving the affixes to new component

--- src/components/theAffixe.jsx
+++ src/components/theAffixe.jsx
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { UncontrolledTooltip } from "reactstrap";
+
+import styles from "./theAffixe.module.css";
 
 export default class TheFooter extends React.Component {
    constructor(props) {
@@ -7,30 +10,47 @@ export default class TheFooter extends React.Component {
 
       this.state = {
          affixes: [
-            { id: "2", link: "../images/skit.jpg" },
-            { id: "3", link: "../images/volc.jpg" },
-            { id: "4", link: "../images/necro.jpg" },
-            { id: "5", link: "../images/teem.jpg" },
-            { id: "6", link: "../images/raging.jpg" },
-            { id: "7", link: "../images/bolst.jpg" },
-            { id: "8", link: "../images/sang.jpg" },
-            { id: "9", link: "../images/tyra.jpg" },
-            { id: "10", link: "../images/forti.jpg" },
-            { id: "11", link: "../images/burst.jpg" },
-            { id: "12", link: "../images/griev.jpg" },
-            { id: "13", link: "../images/expl.jpg" },
-            { id: "14", link: "../images/quak.jpg" },
-            { id: "117", link: "../images/reap.jpg" }
-         ]
+            { id: 2, link: "../images/affixes/skit.jpg" },
+            { id: 3, link: "../images/affixes/volc.jpg" },
+            { id: 4, link: "../images/affixes/necro.jpg" },
+            { id: 5, link: "../images/affixes/teem.jpg" },
+            { id: 6, link: "../images/affixes/raging.jpg" },
+            { id: 7, link: "../images/affixes/bolst.jpg" },
+            { id: 8, link: "../images/affixes/sang.jpg" },
+            { id: 9, link: "../images/affixes/tyra.jpg" },
+            { id: 10, link: "../images/affixes/forti.jpg" },
+            { id: 11, link: "../images/affixes/burst.jpg" },
+            { id: 12, link: "../images/affixes/griev.jpg" },
+            { id: 13, link: "../images/affixes/expl.jpg" },
+            { id: 14, link: "../images/affixes/quak.jpg" },
+            { id: 117, link: "../images/affixes/reap.jpg" }
+         ],
+         run: this.props.run
       };
    }
 
    render() {
       return (
          <Fragment>
-            {this.state.affixes.map((pics, u) => {
-               if (runs.affixes === pics.name) {
-                  return <img style={{ minHeight: "200px" }} src={pics.link} />;
+            {this.state.run.affixes.map(runAff => {
+               {
+                  return this.state.affixes.map((aff, i) => {
+                     const affixId = `affix${runAff.id}-${i}-${Math.ceil(Math.random() * 10000)}`;
+                     return runAff.id === aff.id ? (
+                        <Fragment>
+                           <a href={`https://wowhead.com/affix=` + runAff.id}>
+                              <img src={aff.link} className={styles.affixe} id={affixId} />
+                           </a>
+                           {/* BUG ZONE
+                              Tooltip doesn't apply to duplicate affixes image
+                           */}
+                           <UncontrolledTooltip placement="top" target={affixId}>
+                              {runAff.description}
+                           </UncontrolledTooltip>
+                           {/* ENDING BUG ZONE */}
+                        </Fragment>
+                     ) : null;
+                  });
                }
             })}
          </Fragment>
